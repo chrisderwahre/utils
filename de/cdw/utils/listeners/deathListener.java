300a402698bd78489ba4924694655961e9723bce
ChrisderWahre <chrissico@outlook.de>
2018-03-27 16:05:35 +0200

    Updated deathListener

--- de/cdw/utils/listeners/deathListener.java
+++ de/cdw/utils/listeners/deathListener.java
@@ -1,13 +1,24 @@
 package de.cdw.utils.listeners;
 
 import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
+import de.cdw.utils.cdwUtils;
+
 public class deathListener implements Listener{
 	
+public cdwUtils plugin;
+	
+	public deathListener(cdwUtils instance){
+        plugin = instance;
+    }
+	
+	@EventHandler
 	public void death(PlayerDeathEvent e) {
 		e.setDeathMessage(ChatColor.GOLD + e.getEntity().getName() + ChatColor.GRAY + " ist gestorben!");
 	}
 
 }
+
